3b1725f5ea90709b078d05ae1ade030bfbb05578
aqsaprmn <aqshapermana@gmail.com>
2022-05-10 15:11:25 +0700

    Config Baru

---
+++ system/config.php
@@ -0,0 +1,8 @@
+<?php
+$db_host        = 'localhost';
+$db_user        = 'root';
+$db_password    = '';
+$db_name        = 'am_pario';
+//define('APP_URL', 'http://192.168.3.17/apipinter');
+define('APP_URL', 'http://localhost/alpro');
+$_app_stage = 'Live';
