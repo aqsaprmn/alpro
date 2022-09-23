e2c75e5cd37d3a9aeac0dd73e54b9276baeb44ff
aqsaprmn <aqshapermana@gmail.com>
2022-09-23 10:28:48 +0700

    Database

--- system/config.php
+++ system/config.php
@@ -8,7 +8,7 @@
 $db_host        = 'localhost';
 $db_user        = 'billing';
 $db_password    = 'W1fipinter';
-$db_name        = 'am_pario';
+$db_name        = 'am';
 define('APP_URL', 'http://103.242.181.10/alpro');
 
 $_app_stage = 'Live';
